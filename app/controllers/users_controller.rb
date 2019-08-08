d65b8a71218354d8c7882ddb11c6e54b8b7d4e87
kthatoto <kthatoto@gmail.com>
2019-08-09 01:55:44 +0900

    Bundle method of set user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,50 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:update, :show]
+
   def create
     user = User.create!
     render json: { user: user }, status: 201
   end
 
   def update
+    @user.update(update_params)
+    render json: { user: @user }
   end
 
   def show
-    user = User.find_by!(code: params[:code])
-    mobile_user = user.mobile_user
-    room = user.room
-    users = room ? room.users.where.not(id: user.id) : []
+    mobile_user = @user.mobile_user
+    room = @user.room
+    opposite_user = room ? room.users.where.not(id: @user.id).first : nil
 
     if room.nil? || room.preparing
       keys = ""
-      keys = room ? room.keys : user.keys
+      keys = room ? room.keys : @user.keys
       return render json: {
-        users: users,
+        opposite_user: opposite_user,
         keys: keys,
         mobile_user: mobile_user,
         preparing: true,
-        hosting: user.hosting,
+        hosting: @user.hosting,
       }
     else
       render json: {
-        users: users,
+        opposite_user: opposite_user,
         room: room,
-        user: user,
+        user: @user,
         mobile_user: mobile_user,
         preparing: false
       }
     end
   end
+
+  private
+
+    def set_user
+      @user = User.find_by!(code: params[:code])
+    end
+
+    def update_params
+      params.permit(:peer_id)
+    end
 end
